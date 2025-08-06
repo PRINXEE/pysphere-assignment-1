6dff5a8ecab3c625f784099e49285bdce9a23244
Muhammad .S. Gimba <42121415+PRINXEE@users.noreply.github.com>
2025-08-06 23:38:51 +0100

    Initial commit

---
+++ ohms_law.py
@@ -0,0 +1,6 @@
+# voltage calculator by Muhammad Sabiu Gimba
+print('Welcome to voltage (V) calculator using Ohms law')
+I = int(input("Enter the current (in A): "))
+R = int(input("Enter the resistance (in Ohms): "))
+v = I * R
+print('The voltage is:', v, 'volts')
